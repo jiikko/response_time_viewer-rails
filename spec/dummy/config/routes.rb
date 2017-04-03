0cb44938031cf14f780b836e9dba034f02d14e8a
jiikko <n905i.1214@gmail.com>
2017-04-04 07:52:08 +0900

    return 200 responce status

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  mount ResponseTimeViewer::Rails::Engine => "/response_time_viewer-rails"
+  mount ResponseTimeViewer::Rails::Engine => "/response_time_viewer", as: :response_time_viewer
+  get 'response_time_viewer' => 'response_time_viewer#index'
 end
