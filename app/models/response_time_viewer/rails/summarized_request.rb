0b0d4b635f569e40878e5fd945df02dbe4062c85
jiikko <n905i.1214@gmail.com>
2017-04-06 08:06:26 +0900

    5万件ごとにバルクインサートする

--- app/models/response_time_viewer/rails/summarized_request.rb
+++ app/models/response_time_viewer/rails/summarized_request.rb
@@ -9,24 +9,39 @@ class ResponseTimeViewer::Rails::SummarizedRequest < ResponseTimeViewer::Rails::
 
   # 一度に26万件入ったが1万件ずつにわけたい
   def self.import_from_file(file)
-    list = file.each_line.map do |line|
-      hash = JSON.parse(line)
-      splited = hash['path'].split('?')
-      path_without_params = splited[0] && splited[0][0..190]
-      params = splited[1] && splited[1][0..190]
-      self.new(
-        path: path_without_params,
-        params: params,
-        summarized_at: Time.parse(hash['time']),
-        device: hash['user_agent'],
-        merged_count: hash['merged_count'],
-        total_ms: hash['total_ms'],
-        view_ms: hash['mss']['Views'] || 0,
-        ar_ms: hash['mss']['ActiveRecord'] || 0,
-        solr_ms: hash['mss']['Solr'] || 0,
-      )
+    loop do
+      summarized_requests = []
+      got_eof = false
+      50000.times do
+        line = nil
+        begin
+          line = file.readline
+        rescue EOFError
+          got_eof = true
+        end
+        if got_eof
+          hash = JSON.parse(line)
+          splited = hash['path'].split('?')
+          path_without_params = splited[0] && splited[0][0..190]
+          params = splited[1] && splited[1][0..190]
+          summarized_requests << self.new(
+            path: path_without_params,
+            params: params,
+            summarized_at: Time.parse(hash['time']),
+            device: hash['user_agent'],
+            merged_count: hash['merged_count'],
+            total_ms: hash['total_ms'],
+            view_ms: hash['mss']['Views'] || 0,
+            ar_ms: hash['mss']['ActiveRecord'] || 0,
+            solr_ms: hash['mss']['Solr'] || 0,
+          )
+        else
+          break
+        end
+      end
+      self.import(summarized_requests)
+      break if got_eof
     end
-    self.import(list)
   end
 
   def path_with_params
