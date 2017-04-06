3af128c59251aaecda9e9ee4b05f1b7e01f81e89
jiikko <n905i.1214@gmail.com>
2017-04-06 23:29:22 +0900

    fix spec

--- app/models/response_time_viewer/rails/summarized_request.rb
+++ app/models/response_time_viewer/rails/summarized_request.rb
@@ -11,36 +11,34 @@ class ResponseTimeViewer::Rails::SummarizedRequest < ResponseTimeViewer::Rails::
   def self.import_from_file(file)
     loop do
       summarized_requests = []
-      got_eof = false
+      continuing_read_file = true
       50000.times do
         line = nil
         begin
           line = file.readline
         rescue EOFError
-          got_eof = true
-        end
-        if got_eof
-          hash = JSON.parse(line)
-          splited = hash['path'].split('?')
-          path_without_params = splited[0] && splited[0][0..190]
-          params = splited[1] && splited[1][0..190]
-          summarized_requests << self.new(
-            path: path_without_params,
-            params: params,
-            summarized_at: Time.parse(hash['time']),
-            device: hash['user_agent'],
-            merged_count: hash['merged_count'],
-            total_ms: hash['total_ms'],
-            view_ms: hash['mss']['Views'] || 0,
-            ar_ms: hash['mss']['ActiveRecord'] || 0,
-            solr_ms: hash['mss']['Solr'] || 0,
-          )
-        else
+          continuing_read_file = false
+          file.close
           break
         end
+        hash = JSON.parse(line)
+        splited = hash['path'].split('?')
+        path_without_params = splited[0] && splited[0][0..190]
+        params = splited[1] && splited[1][0..190]
+        summarized_requests << self.new(
+          path: path_without_params,
+          params: params,
+          summarized_at: Time.parse(hash['time']),
+          device: hash['user_agent'],
+          merged_count: hash['merged_count'],
+          total_ms: hash['total_ms'],
+          view_ms: hash['mss']['Views'] || 0,
+          ar_ms: hash['mss']['ActiveRecord'] || 0,
+          solr_ms: hash['mss']['Solr'] || 0,
+        )
       end
       self.import(summarized_requests)
-      break if got_eof
+      break unless continuing_read_file
     end
   end
 
