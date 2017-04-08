d459b74c804272087bab004377b2e7ab855bec7c
jiikko <n905i.1214@gmail.com>
2017-04-08 20:15:20 +0900

    Impliment 日付で絞りこむ機能

--- app/models/response_time_viewer/rails/search_object.rb
+++ app/models/response_time_viewer/rails/search_object.rb
@@ -1,11 +1,12 @@
 class ResponseTimeViewer::Rails::SearchObject
   include ActiveModel::Model
 
-  attr_accessor :device, :path
+  attr_accessor :device, :path, :start_on, :end_on
 
   def initialize(params)
     super(params)
     @relation = ResponseTimeViewer::Rails::SummarizedRequest.all
+    reflection_form_attributes!
   end
 
   def add_condition!(watching_urls: )
@@ -36,5 +37,19 @@ class ResponseTimeViewer::Rails::SearchObject
 
   def period
     []
+  end
+
+  private
+
+  def reflection_form_attributes!
+    if self.start_on.nil? && self.end_on.nil?
+      self.start_on = 1.week.ago
+      self.end_on = Date.today
+    end
+    if self.start_on.is_a?(String) && self.end_on.is_a?(String)
+      self.start_on = self.start_on.to_date
+      self.end_on = self.end_on.to_date
+    end
+    @relation.where!(summarized_at: self.start_on..self.end_on)
   end
 end
