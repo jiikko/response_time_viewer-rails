d41739d8383039b4ee4290a713f036b9fd6019d7
jiikko <n905i.1214@gmail.com>
2017-04-10 19:02:28 +0900

    fix

--- app/models/response_time_viewer/rails/watching_url_group.rb
+++ app/models/response_time_viewer/rails/watching_url_group.rb
@@ -1,5 +1,5 @@
 module ResponseTimeViewer::Rails
   class WatchingUrlGroup < ApplicationRecord
-    has_and_belongs_to_many :watching_urls, dependent: :destroy
+    has_and_belongs_to_many :watching_urls
   end
 end
