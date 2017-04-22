be7f18bdb6aed815dc3d61d427eafbe2d01a0935
jiikko <n905i.1214@gmail.com>
2017-04-22 21:00:52 +0900

    URLの追加削除をajaxで行う

--- app/controllers/response_time_viewer/rails/watching_url_groups/watching_urls_controller.rb
+++ app/controllers/response_time_viewer/rails/watching_url_groups/watching_urls_controller.rb
@@ -1,24 +1,49 @@
 module ResponseTimeViewer::Rails
   class WatchingUrlGroups::WatchingUrlsController < ApplicationController
+
     def index
       @watching_url_group = WatchingUrlGroup.find(params[:watching_url_group_id])
+      set_variables
+    end
+
+    def create
+      @watching_url_group = WatchingUrlGroup.find(params[:watching_url_group_id])
+      @watching_url_group.watching_urls << WatchingUrl.find(params[:watching_url_id])
+      respond_to do |format|
+        format.js do
+          set_variables
+          @body = render_to_string('response_time_viewer/rails/watching_url_groups/watching_urls/index', layout: false)
+          render :rerender
+        end
+        format.html do
+          redirect_to @watching_url_group_watching_urls_path, notice: 'URLを登録しました'
+        end
+      end
+    end
+
+    def destroy
+      @watching_url_group = WatchingUrlGroup.find(params[:watching_url_group_id])
+      ids = @watching_url_group.watching_url_ids
+      @watching_url_group.watching_url_ids = ids.delete_if { |x| x == params[:id].to_i }
+      respond_to do |format|
+        format.js do
+          set_variables
+          @body = render_to_string('response_time_viewer/rails/watching_url_groups/watching_urls/index', layout: false)
+          render :rerender
+        end
+        format.html do
+          redirect_to watching_url_group_watching_urls_path, notice: 'URLを削除しました'
+        end
+      end
+    end
+
+    private
+
+    def set_variables
       @watching_urls = @watching_url_group.watching_urls
       @addable_watching_urls = WatchingUrl.where.not(id: @watching_urls).
         page(params[:page]).
         per(200)
-    end
-
-    def create
-      watching_url_group = WatchingUrlGroup.find(params[:watching_url_group_id])
-      watching_url_group.watching_urls << WatchingUrl.find(params[:watching_url_id])
-      redirect_to watching_url_group_watching_urls_path, notice: 'URLを登録しました'
-    end
-
-    def destroy
-      watching_url_group = WatchingUrlGroup.find(params[:watching_url_group_id])
-      ids = watching_url_group.watching_url_ids
-      watching_url_group.watching_url_ids = ids.delete_if { |x| x == params[:watching_url_id].to_i }
-      redirect_to watching_url_group_watching_urls_path, notice: 'URLを削除しました'
     end
   end
 end
--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -11,4 +11,6 @@
 // about supported directives.
 //= require Chart.bundle
 //= require chartkick
+//= require jquery
+//= require jquery_ujs/
 //= require_tree .
